989be8183bc3ed18e85129730ea9ff0ff3b070dd
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-31 12:39:03 +0000

    join, spam処理を入れておくフォルダ

---
+++ function/joiner.py
@@ -0,0 +1,17 @@
+import aiohttp
+
+async def joinah(inviteCode):
+    async with aiohttp.ClientSession() as session:
+        with open("data/account.txt") as f:
+            tokens = f.read().splitlines()
+        
+        for token in tokens:
+            headers = {
+                'authority': 'api.revolt.chat',
+                'origin': 'https://app.revolt.chat',
+                'referer': 'https://app.revolt.chat/',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.3',
+                'x-session-token': token
+            }
+            async with session.post(f'https://api.revolt.chat/invites/{inviteCode}', headers=headers, json={}) as res:
+                print(f"[JOINER] {'Joined' if res.status == 200 else 'Failed'} Server on token ---> {token}")
---
+++ function/spammer.py
@@ -0,0 +1,30 @@
+import aiohttp
+import asyncio
+import json
+
+async def send_message(session, channelid, msg, token):
+    headers = {
+        'authority': 'api.revolt.chat',
+        'origin': 'https://app.revolt.chat',
+        'referer': 'https://app.revolt.chat/',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.3',
+        'x-session-token': token
+    }
+
+    async with session.post(f'https://api.revolt.chat/channels/{channelid}/messages', headers=headers, json={'content': msg}) as res:
+        response_text = await res.text()
+        if res.status == 200:
+            print(f"[SPAMMER] Sent message in {channelid} on token ---> {token[:10]}...")
+        else:
+            print(f"[SPAMMER] Failed to send message in {channelid} on token ---> {token[:10]}...")
+
+async def spammer(msg, count):
+    async with aiohttp.ClientSession() as session:
+        with open("data/account.txt") as f:
+            tokens = f.read().splitlines()
+        with open("data/channels.txt") as f:
+            channels = f.read().splitlines()
+
+        for i in range(count):
+            tasks = [send_message(session, channel, msg, token) for channel in channels for token in tokens]
+            await asyncio.gather(*tasks)
